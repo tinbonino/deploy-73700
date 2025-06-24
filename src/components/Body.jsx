c730f1f0a28e9dad0149f0aca4f51a1587188a6b
martin <tinbonino@gmail.com>
2025-06-23 21:50:02 -0300

    Actualizacion de body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,6 +5,7 @@ function Body() {
     <div>
         <h1>Aprendemos context alegremente</h1>
         <h2>{import.meta.env.VITE_MENSAJE}</h2>
+        <p>Espero que hayan disfrutado la cursada, fueron 8 clases muy intensas</p>
     </div>
   )
 }
